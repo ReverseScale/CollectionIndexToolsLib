20e2dd5b13075060014d9410e8dc30db6e05a7d5
XieTian <Stevens@Stevens-MacBook-Air.local>
2018-03-21 10:54:00 +0800

    🚀:Deploying stuff.

---
+++ Example/Pods/Target Support Files/CollectionIndexToolsLib/CollectionIndexToolsLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CollectionIndexToolsLib : NSObject
+@end
+@implementation PodsDummy_CollectionIndexToolsLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-CollectionIndexToolsLib_Example/Pods-CollectionIndexToolsLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CollectionIndexToolsLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CollectionIndexToolsLib_Example
+@end
